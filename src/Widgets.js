d2ce692b40b57f4edab666601676e852a6e71a55
dwanerich <dwanerich@gmail.com>
2021-03-28 14:04:10 -0400

    removed default border and styled twitter search bar in widgets column

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import './Widgets.css'
+// import { Timeline, Share, TWeet } from 'react-twitter-widgets'
+import { Tweet, Timeline, Share } from 'react-twitter-widgets'
+
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
     return (
         <div className="widgets">
-                    <h2>Widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>The latest</h2>
+
+                <Tweet tweetId="1367202325744525322" />
+                <Timeline
+                    dataSource={{
+                    sourceType: 'profile',
+                    screenName: '1batdotus'
+                }}
+                    options={{
+                    height: '400'
+                }}
+                />
+
+                <Share
+                url={"http://linkedin.com/in/dwanerich"}
+                options={{ text: "#Jack'd the Big homie", via: "http://dwanerich.com"}}
+                />
+                
+
+            </div>
+            
         </div>
-    )
+    );
 }
 
 
